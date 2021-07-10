3b1477fcf730a478e46b9e6d614545d0e6daacb5
prosanna <prosanna2005@gmail.com>
2021-07-10 17:33:31 +0530

    Initial commit for Movie Info Service.

---
+++ src/test/java/io/microServiceWorkShop/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.microServiceWorkShop.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
